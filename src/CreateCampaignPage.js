0594ecfdc9a325804274c597b13ca3a53a9d6192
fahraday <ghosterinc@gmail.com>
2021-06-29 22:29:34 -0400

    adding short information to the form

--- src/CreateCampaignPage.js
+++ src/CreateCampaignPage.js
@@ -1,10 +1,12 @@
 import { CreateCampaign } from "./components/createCampaign/CreateCampaign";
 
+const CampaignFields = [{ label: "campaign name", type: "text", autoFocus: true }, { label: "funding goal", type: "number" }, { label: "deadline", type: "date" }, { label: "funding cap", type: "number" }, { label: "beneficiary", type: "text" }]
+
 export const CreateCampaignPage = () => {
 
     return (
         <>
-            <CreateCampaign/>
+            <CreateCampaign CampaignFields={CampaignFields}/>
         </>
     )
 }
--- src/components/createCampaign/CreateCampaign.js
+++ src/components/createCampaign/CreateCampaign.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react'
 import { Button } from '../Buttons'
 
-import { Container, Wrapper, FormContent, FormImage, ImageWrapper, FormInputImage, Form, FormShortsFields, ShortFieldsWrapper, FormLabel, FormInput, FormLargeFields, LargerFieldsWrapper, TextArea, TextDescription, TextTittle, TopLine } from './CreateCampaign.styles'
+import {
+  Container,
+  Wrapper,
+  FormContent,
+  FormImage,
+  ImageWrapper,
+  FormInputImage,
+  Form,
+  FormShortsFields,
+  ShortFieldsWrapper,
+  FormLabel,
+  FormInput,
+  FormLargeFields,
+  LargerFieldsWrapper,
+  TextArea,
+  TextDescription,
+  TextTittle,
+  TopLine
+} from './CreateCampaign.styles'
 
-const CampaignFields = [{label: "campaign name", type: "text"},{label: "funding goal", type: "number"}, {label: "deadline", type: "date"}, {label: "funding cap", type: "number"}, {label: "beneficiary", type: "text"}]
 
 
-export const CreateCampaign = () => {
+
+export const CreateCampaign = ({ CampaignFields }) => {
 
   return (
     <Container>
@@ -21,11 +39,15 @@ export const CreateCampaign = () => {
           {/* </FormImage> */}
 
           <Form>
-          <TopLine>Your Campain</TopLine>
+            <TopLine>Your Campain</TopLine>
             <FormShortsFields>
               <ShortFieldsWrapper>
-                <FormLabel >Campaign Name</FormLabel>
-                <FormInput type="text" autoFocus={true} />
+                {CampaignFields.map(({ label, type, autoFocus }) => (
+                  <>
+                <FormLabel >{label}</FormLabel>
+                <FormInput type={type} autoFocus={autoFocus} />
+                </>
+                ))}
                 {/* <input type="image"> */}
 
               </ShortFieldsWrapper>
@@ -35,9 +57,9 @@ export const CreateCampaign = () => {
             <FormLargeFields>
 
               <LargerFieldsWrapper>
-                <TextTittle placeholder="Tittle" type="text" maxLength="60"/>
-                <TextDescription placeholder="Short Description" type="text" maxLength="220"/>
-                <TextArea placeholder ="Write your history" type="text"/>
+                <TextTittle placeholder="Tittle" type="text" maxLength="60" />
+                <TextDescription placeholder="Short Description" type="text" maxLength="220" />
+                <TextArea placeholder="Write your history" type="text" />
               </LargerFieldsWrapper>
 
             </FormLargeFields>
